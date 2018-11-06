dd2d6855e46d4829e7199a0b325225abf3ed01fa
Niloy Rahman <havocisgod@gmail.com>
2018-11-06 22:25:21 +0600

    Add files via upload

---
+++ Problem5.java
@@ -0,0 +1,15 @@
+
+public class Problem5 {
+public static void main(String[] args) {
+	double sum=0;
+	
+	for(int i=1;i<=97;i=i+2) {
+		sum=sum + (i*1.0)/(i+2);
+		
+		
+	}
+	
+	System.out.println(sum);
+		
+}
+}
